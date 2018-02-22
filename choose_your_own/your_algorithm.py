7c117c2ed821f3dcfa3621425da04334135098a1
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2018-02-22 20:01:09 +0200

    tmp

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,8 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -17,28 +19,46 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+#10 5 9
+clf = RandomForestClassifier(min_samples_split=10, n_estimators=5,max_depth=9)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+acu = accuracy_score(labels_test, pred)
+print acu
+# m = 0.928
+# for split in range(10, 60, 5):
+#     for estim in range(5, 50, 3):
+#         for d in range(4, 10):
+#
+#             clf = RandomForestClassifier(min_samples_split=split, n_estimators=estim,max_depth=d)
+#             clf.fit(features_train, labels_train)
+#             pred = clf.predict(features_test)
+#
+#             acu = accuracy_score(labels_test, pred)
+#             print split, estim, d
+#             print acu
+#             if acu > m:
+#                 m = acu
 
 
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+# print m
+prettyPicture(clf, features_test, labels_test)
+# try:
+#
+# except NameError:
+#     pass
--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,15 +12,20 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
+acu = accuracy_score(labels_test, pred)
+print acu
 
 #########################################################
 ### your code goes here ###
